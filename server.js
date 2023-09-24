d8166487d18fd9d17ecfe281ed74159b1c1d142b
cyan-byte <62404444+cyan-byte@users.noreply.github.com>
2023-09-23 20:40:03 -0400

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const express = require("express");
 const app = express();
 //  the following lines of code are needed for our application to be able to use the JSX View Engine
 const jsxEngine = require("jsx-view-engine");
+// Define a port number for your server
+const port = 3000;
+// Set this 'database' to a variable called pokemon in your server.jsfile
+const pokemon = require("./models/pokemon");
+
 app.set("view engine", "jsx");
 app.engine("jsx", jsxEngine());
 
-// Define a port number for your server
-const port = 3000;
-
-// Set this 'database' to a variable called pokemon in your server.jsfile
-const pokemon = require("./models/pokemon");
 // Create a get route /pokemon that will res.send(pokemon), which will display your pokemon data as json in the browser
 app.get("/pokemon", (req, res) => {
   // Render the Index.jsx file
-  res.render("Index");
+  res.render("Index", { pokemon: pokemon });
 });
 
 // define a route that sends a welcome message
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
-// Start the server and listen on teh specified port
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- views/Index.jsx
+++ views/Index.jsx
@@ -2,7 +2,7 @@
 const React = require("react");
 
 // Define your JSX component
-const Index = () => {
+const Index = ({ pokemon }) => {
   // Inline styles as an object
   const headerStyle = {
     textAlign: "center",
@@ -10,6 +10,17 @@ const Index = () => {
     color: "white",
     padding: "20px",
   };
+
+  // Capitalize the first letter of a string
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  // Process the Pokémon data to capitalize names
+  const capitalizedPokemonData = pokemon.map((pokemon) => ({
+    ...pokemon,
+    name: capitalizeFirstLetter(pokemon.name),
+  }));
 
   return (
     <html>
@@ -21,6 +32,11 @@ const Index = () => {
           {/* Inline styles applied to the <h1> element */}
           <h1 style={headerStyle}>See All The Pokemon!</h1>
         </div>
+        <ul>
+          {capitalizedPokemonData.map((pokemon, index) => (
+            <li key={index}>{pokemon.name}</li>
+          ))}
+        </ul>
       </body>
     </html>
   );
